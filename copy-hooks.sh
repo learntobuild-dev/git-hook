874b825a95aa42e3f53853250942bd8c8d658df7
timothy <timothy@learntobuild.dev>
2022-11-13 17:49:57 +0200

    deleting the copy-hooks script

--- copy-hooks.sh
+++
@@ -1,9 +0,0 @@
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook ../calculator/.git/hooks/pre-commit
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook ../calculator/.git/hooks/prepare-commit-msg
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook ../calculator/.git/hooks/commit-msg
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook ../calculator/.git/hooks/post-checkout
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook ../calculator/.git/hooks/pre-rebase
-
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook /home/timothy/gogs-repositories/timothy/calculator.git/hooks/pre-receive
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook /home/timothy/gogs-repositories/timothy/calculator.git/hooks/update
-cp ./bin/Debug/net6.0/linux-x64/publish/git-hook /home/timothy/gogs-repositories/timothy/calculator.git/hooks/post-receive
